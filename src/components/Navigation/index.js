1654dde0ef18dc9aafab2980cd395b7095259acf
Sean <seankim912@yahoo.com>
2023-12-21 23:33:40 -0600

    navbar started

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+    return (
+        <ul className="nav-container">
+            <li></li>
+            <li></li>
+        </ul>
+    )
+}
