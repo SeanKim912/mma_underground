c7202726ad9269007ab16a65e6b5be34c1ace384
Sean <seankim912@yahoo.com>
2024-01-03 18:07:13 -0600

    cleaned up template, navbar placed

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import './Navigation.css'
 
 const Navigation = () => {
     return (
         <ul className="nav-container">
-            <li></li>
-            <li></li>
+            <li>CLASSES</li>
+            <li>KIDS</li>
+            <li>COACHES</li>
+            <li>TESTIMONIALS</li>
+            <li>ABOUT US</li>
+            <li>CONTACT US</li>
         </ul>
     )
 }
+
+export default Navigation;
