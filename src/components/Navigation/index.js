f62649caf39596b26c74d4290ac17aca5e2888d5
Sean <seankim912@yahoo.com>
2024-01-04 22:14:24 -0600

    navbar formatted

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,12 +5,17 @@ import './Navigation.css'
 const Navigation = () => {
     return (
         <ul className="nav-container">
-            <li>CLASSES</li>
-            <li>KIDS</li>
-            <li>COACHES</li>
-            <li>TESTIMONIALS</li>
-            <li>ABOUT US</li>
-            <li>CONTACT US</li>
+            <li>
+                <img className="logo" src="https://lh3.googleusercontent.com/pw/ABLVV87_C5HNi3tViTLh00DNnEvoEiDgvz50C11-Hh-4irLkZHw6u42r4scYVk2t-zqT4ZyAK1D_lwpugwVke6yi-rqyMkOyTpX-2-j1SzH5aekwdDbgOR21kj-LVxPVYJyE1S3UC8jYjJcZ4ElVzRden0D4=w192-h189-s-no-gm?authuser=0" />
+            </li>
+            <li className="menu-tabs">
+                <li className="tab">CLASSES</li>
+                <li className="tab">KIDS</li>
+                <li className="tab">COACHES</li>
+                <li className="tab">TESTIMONIALS</li>
+                <li className="tab">ABOUT US</li>
+                <li className="tab">CONTACT US</li>
+            </li>
         </ul>
     )
 }
