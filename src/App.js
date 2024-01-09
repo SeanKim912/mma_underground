8a399fb4379e9976b0a9d851165d8b57444c25e8
Sean <seankim912@yahoo.com>
2024-01-08 18:10:18 -0600

    switch not importing properly from react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
     <>
       <Navigation />
+      <Switch>
+        <Route exact path="/">
+          <MainPage />
+        </Route>
+      </Switch>
     </>
   );
 }
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -6,7 +6,7 @@ const Navigation = () => {
     return (
         <ul className="nav-container">
             <li>
-                <img className="logo" src="https://lh3.googleusercontent.com/pw/ABLVV87_C5HNi3tViTLh00DNnEvoEiDgvz50C11-Hh-4irLkZHw6u42r4scYVk2t-zqT4ZyAK1D_lwpugwVke6yi-rqyMkOyTpX-2-j1SzH5aekwdDbgOR21kj-LVxPVYJyE1S3UC8jYjJcZ4ElVzRden0D4=w192-h189-s-no-gm?authuser=0" />
+                <img className="logo" src={require('./mma_underground_icon.png')} />
             </li>
             <li className="menu-tabs">
                 <li className="tab">CLASSES</li>
