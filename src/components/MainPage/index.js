15e70e1a18131c1d1766f133e04c1488335d552d
Sean <seankim912@yahoo.com>
2024-01-22 17:17:43 -0600

    buttons, text formatted

--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -5,9 +5,18 @@ const MainPage = () => {
     return (
         <div className="main-container">
             <div className="main-left">
-                <h1>MMA UNDERGROUND</h1>
+                <h1 className="main-text">MMA UNDERGROUND</h1>
                 <h3>835 Virginia Rd, Unit C</h3>
                 <h3>Crystal Lake, Illinois 60014</h3>
+                <h3>(847) 219-5920</h3>
+                <ul>
+                    <li>
+                        <button>CONTACT US</button>
+                    </li>
+                    <li>
+                        <button>CLASS SCHEDULE</button>
+                    </li>
+                </ul>
             </div>
             <div className="main-right">
                 <img className="main-img" src={require('./mma_underground_banner.webp')} />
