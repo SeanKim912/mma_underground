c972df5bd8c29f73e342433035892e9aa361f6cd
Sean <seankim912@yahoo.com>
2024-01-21 21:52:24 -0600

    main page tiled and proportioned

--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -3,9 +3,16 @@ import './MainPage.css'
 
 const MainPage = () => {
     return (
-        <div>
-            <img src={require('./northwest_herald.webp')} />
-            <img src={require('./mma_underground_banner.webp')} />
+        <div className="main-container">
+            <div className="main-left">
+                <h1>MMA UNDERGROUND</h1>
+                <h3>835 Virginia Rd, Unit C</h3>
+                <h3>Crystal Lake, Illinois 60014</h3>
+            </div>
+            <div className="main-right">
+                <img className="main-img" src={require('./mma_underground_banner.webp')} />
+                <img className="main-img" src={require('./northwest_herald.webp')} />
+            </div>
         </div>
     )
 }
