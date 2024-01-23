a1d9be9bfebf1e94aababa3d0dc816489cc59724
Sean <seankim912@yahoo.com>
2024-01-22 18:07:24 -0600

    react slideshow component installed and initialized

---
+++ src/components/MainGallery/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './MainGallery.css';
+import { Slide } from "react-slideshow-image";
+
+const MainGallery = () => {
+    return (
+        <>
+            <Slide>
+
+            </Slide>
+        </>
+    )
+}
+
+export default MainGallery;
--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import './MainPage.css'
+import './MainPage.css';
+import MainGallery from "../MainGallery";
 
 const MainPage = () => {
     return (
