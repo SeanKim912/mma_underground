332c83e0fd30b39f533719a835807ceb8de9e6c2
Sean <seankim912@yahoo.com>
2024-01-24 17:15:55 -0600

    images loading properly, in order, but overall gallery needs formatting

--- src/components/MainGallery/index.js
+++ src/components/MainGallery/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './MainGallery.css';
 import { Slide } from "react-slideshow-image";
+
 import img1 from './mma_underground_banner.webp';
-import img2 from './mma_underground_banner.webp';
+import img2 from './northwest_herald.webp';
+import img3 from './gym_front.jpg';
 
 const MainGallery = () => {
     const images = [
         img1,
-        img2
+        img2,
+        img3
     ]
 
     return (
@@ -15,12 +18,17 @@ const MainGallery = () => {
             <Slide>
                 <div className='slide-effect'>
                     <div style={{ 'backgroundImage': `url(${images[0]})` }}>
-                        <span>Slide 2</span>
+                        <span>Slide 1</span>
                     </div>
                 </div>
                 <div className='slide-effect'>
                     <div style={{ 'backgroundImage': `url(${images[1]})` }}>
                         <span>Slide 2</span>
+                    </div>
+                </div>
+                <div className='slide-effect'>
+                    <div style={{ 'backgroundImage': `url(${images[2]})` }}>
+                        <span>Slide 3</span>
                     </div>
                 </div>
             </Slide>
