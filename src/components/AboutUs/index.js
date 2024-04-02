b45c64c964eeb1ce2f865691f76206301666fcda
Sean <seankim912@yahoo.com>
2024-04-01 21:29:13 -0500

    grid added for about us

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -25,6 +25,58 @@ const AboutUs = () => {
                 Then join MMA Underground today!
             </p>
             <h3 className='aboutUsText'>NO MATTER WHAT YOUR GOAL IS, WE WILL MAKE A CHAMPION OUT OF YOU</h3>
+            <div className='gridContainer'>
+                <div className='point'>
+                    <h3 className='pointHeader'>THE PERFECT MIX OF PRACTICAL, HEALTHY AND FUN</h3>
+                    <p className='pointText'>
+                        MMA isn&apos;t just for the pros. Not only will you learn useful, physically demanding moves that will get you in shape, you&apos;ll be doing it alongside
+                        like-minded people who will motivate you and can keep you accountable. No more grinding at the gym alone! There&apos;s always something new to learn, and
+                        we guarantee you&apos;ll have lots of reasons to exercise other than “it&apos;s what you&apos;re supposed to do”.
+                    </p>
+                </div>
+                <div className='point'>
+                    <h3 className='pointHeader'>GREAT FOR THE UNDECIDED</h3>
+                    <p className='pointText'>
+                        Are you excited by the savage power of a Muay Thai kick? Intrigued by the elegant fundamentals of boxing? Would you prefer to dominate your opponents with
+                        the brute physicality of wrestling, or are you a smaller person looking to bypass size and strength with an encyclopedia of jujutsu techniques? No idea at
+                        all? No worries. MMA allows beginners to practice a little bit of everything, so we&apos;ll help find the style for you.
+                    </p>
+                </div>
+                <div className='point'>
+                    <h3 className='pointHeader'>FLEXIBLE IN THE GYM, FLEXIBLE IN LIFE</h3>
+                    <p className='pointText'>
+                        The MMA practitioner faces countless challenges throughout training, demanding a wide variety of skills with every part of the body. You will:
+                            <ul>
+                                <li>Learn to move your body quickly and economically.</li>
+                                <li>Increase your flexibility to provide more range of motion.</li>
+                                <li>Understand proper biomechanics to prevent injury, maintain balance and lift your opponent.</li>
+                                <li>Build mental fortitude to endure fatigue and pain.</li>
+                                <li>Learn to think at a high level when feinting, counter-striking, or executing jujutsu tactics.</li>
+                            </ul>
+                        All these can help you with the unforeseen problems of everyday life.
+                    </p>
+                </div>
+                <div className='point'>
+                    <h3 className='pointHeader'>SELF-DEFENSE AT ITS BEST</h3>
+                    <p className='pointText'>
+                        If you want to feel ready for everything, MMA is your sport. As Oscar Sensei loves to say, “If you punch and kick, I&apos;ll grab. If you grab, I&apos;ll punch and kick.”
+                    </p>
+                </div>
+                <div className='point'>
+                    <h3 className='pointHeader'>IF ONE SPORT GETS YOU IN SHAPE, IMAGINE A MIX OF THREE OR MORE?</h3>
+                    <p className='pointText'>
+                        By bringing together different sports such as jujutsu, muay thai, wrestling, boxing, and old-fashioned physical conditioning, you get a complete workout of every
+                        muscle that would make a triathlete envious.
+                    </p>
+                </div>
+                <div className='point'>
+                    <h3 className='pointHeader'>PROVEN EFFICACY AT THE HIGHEST LEVELS</h3>
+                    <p className='pointText'>
+                    With money on the line that has at times numbered in the millions, elite athletes have spent decades testing what works and what doesn&apos;t. MMA is the ultimate
+                    distillation of the world&apos;s most effective fighting techniques. What we teach here at the gym is what the best use to beat the best, no matter what nation it comes from.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
