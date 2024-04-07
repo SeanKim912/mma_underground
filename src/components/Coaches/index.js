76a8afc01a44971b6cf68e84e2ff762caeb44e63
Sean <seankim912@yahoo.com>
2024-04-06 22:42:36 -0500

    main coaches page set up with animations, columns, etc.

--- src/components/Coaches/index.js
+++ src/components/Coaches/index.js
@@ -4,8 +4,48 @@ import './Coaches.css'
 const Coaches = () => {
     return (
         <div className='coachesContainer'>
-            <img src="https://mmaundergroundbucket.s3.us-east-2.amazonaws.com/oscar.jpg" />
-            <img src="https://mmaundergroundbucket.s3.us-east-2.amazonaws.com/dylan.jpg" />
+            <div className='coachesColumn'>
+                <div
+                    className='coachImage'
+                    style={{
+                        backgroundImage: `url("https://mmaundergroundbucket.s3.us-east-2.amazonaws.com/dylan.jpg")`,
+                        width: "33vw"
+                }}>
+                    <div className="hover-gradient">
+                        <div className="hover-text">
+                            <h2>DYLAN KOWALSKI</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='coachesColumn'>
+                <div
+                    className='coachImage'
+                    style={{
+                        backgroundImage: `url("https://mmaundergroundbucket.s3.us-east-2.amazonaws.com/oscar.jpg")`,
+                        width: "33vw"
+                }}>
+                    <div className="hover-gradient">
+                        <div className="hover-text">
+                            <h2>OSCAR OLVIDARES SENSEI</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='coachesColumn'>
+                <div
+                    className='coachImage'
+                    style={{
+                        backgroundImage: `url("https://mmaundergroundbucket.s3.us-east-2.amazonaws.com/latasha.jpg")`,
+                        width: "33vw"
+                }}>
+                    <div className="hover-gradient">
+                        <div className="hover-text">
+                            <h2>LATASHA MARZOLLA</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
