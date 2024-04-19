f7814225cabbda388023a5404ab5c0bf56570b96
Sean <seankim912@yahoo.com>
2024-04-18 23:09:26 -0500

    grid formatting

--- src/components/ClassSchedule/index.js
+++ src/components/ClassSchedule/index.js
@@ -4,7 +4,15 @@ import './ClassSchedule.css';
 const ClassSchedule = () => {
     return (
         <div className='scheduleContainer'>
-            <div className='scheduleGrid'></div>
+            <div className='scheduleGrid'>
+                <div></div>
+                <div>Monday</div>
+                <div>Tuesday</div>
+                <div>Wednesday</div>
+                <div>Thursday</div>
+                <div>Friday</div>
+                <div>Saturday</div>
+            </div>
         </div>
     )
 }
